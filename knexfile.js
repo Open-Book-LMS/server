387a6e07976903d1d0d26412fc79792af9d14463
Marlena Baker <baker.marlena@gmail.com>
2017-09-19 13:44:32 -0600

    bug fix

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgresql://localhost/openbook'
-  }
-
-
+  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
