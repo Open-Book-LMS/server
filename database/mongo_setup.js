8ff9d69372ad8a878b6188b3ee08d61ca8a6148f
Marlena Baker <baker.marlena@gmail.com>
2017-09-19 21:09:01 -0600

    updated seeds

--- database/mongo_setup.js
+++ database/mongo_setup.js
@@ -63,15 +63,16 @@ router.get('/seed', (req, res) => {
         name: 'Architectural Wonders',
         description: "Architectural sites provide an important window into ancient cultures. Angkor Wat, an architectural treasure in the heart of Cambodia, reflects a civilization that integrated culture, nature, and religion, but the impacts of tourism threaten its future.",
         data: {
-          article_text: "At first glance, it is business as usual at the great sandstone temple of Angkor Wat. Through a drape of evening haze, the ancient Cambodian superstructure sees another batch of tourists process across its moat and marvel at its grandeur. Local teenagers waggle cool drinks in the faces of passers-by and auto-rickshaw or \"tuk-tuk\" drivers loudly vie for business. It looks like what it is – a boom town.
-
-But the modern commercial success of the high-profile complex, on the site of the ancient city of Angkor, may be – literally – on shaky ground.
-
-According to heritage experts carrying out restoration work at the temple, which is one of the biggest sets of religious ruins in the world, a plethora of new hotels, cashing in on the country's near-exponential rise in tourist numbers, is sapping gallons of water from beneath nearby urban areas. They say this could upset the delicate foundations on which Angkor Wat sits and could lead to parts of it – including its famous celestial apsara, or carved nymphs – taking an unheavenly tumble to earth.
-
-Philippe Delanghe, the culture programme specialist at Unesco's Phnom Penh office, said this week: \"There is a very important balance between the sand and water on which the temple is built. And if that balance is taken away then we might have trouble with collapse.
-
-\"The growth in the number of hotels around Angkor Wat has meant that more and more holes are being drilled into the earth to extract water from the water table. And this has profound consequences for this imporant mix.",
+          articl_text:'',
+//           article_text: "At first glance, it is business as usual at the great sandstone temple of Angkor Wat. Through a drape of evening haze, the ancient Cambodian superstructure sees another batch of tourists process across its moat and marvel at its grandeur. Local teenagers waggle cool drinks in the faces of passers-by and auto-rickshaw or \"tuk-tuk\" drivers loudly vie for business. It looks like what it is – a boom town.
+//
+// But the modern commercial success of the high-profile complex, on the site of the ancient city of Angkor, may be – literally – on shaky ground.
+//
+// According to heritage experts carrying out restoration work at the temple, which is one of the biggest sets of religious ruins in the world, a plethora of new hotels, cashing in on the country's near-exponential rise in tourist numbers, is sapping gallons of water from beneath nearby urban areas. They say this could upset the delicate foundations on which Angkor Wat sits and could lead to parts of it – including its famous celestial apsara, or carved nymphs – taking an unheavenly tumble to earth.
+//
+// Philippe Delanghe, the culture programme specialist at Unesco's Phnom Penh office, said this week: \"There is a very important balance between the sand and water on which the temple is built. And if that balance is taken away then we might have trouble with collapse.
+//
+// \"The growth in the number of hotels around Angkor Wat has meant that more and more holes are being drilled into the earth to extract water from the water table. And this has profound consequences for this imporant mix.",
         },
         tool_type: "ART",
         course_id: 1,
@@ -110,17 +111,17 @@ Philippe Delanghe, the culture programme specialist at Unesco's Phnom Penh offic
         description: 'Development affects the colonial architecture of an old city in Southeast Asia altering the city’s infrastructure. The city is being rebuilt and is in need of cultural preservation.',
         data: {
             articl_text: '',
-          article_text: "Myanmar has opened up its political system, improved its relationship with the West and ushered in a real estate boom in the past several years. That's mostly good news for Yangon, but not for its remarkable architectural heritage, which has come into the cross hairs of developers trying to cash in on rising land prices.
-
-\"These buildings are priceless,\" says Tint Lwin, who has taught English in a colonial-era building along the city's Pansodan Street for more than three decades. The building, constructed by a Baghdadi Jewish trader around 1906, has ocher-colored walls and Corinthian columns.
-
-Tint Lwin loves the atmosphere of the neighborhood, but worries it won't last. A modern mid-rise is going up across the road. The walls in his building are pitted with black mold, and rain has saturated some ceilings, leaving gaping holes.
-\"I feel very unhappy because of the negligence,\" says Tint Lwin, who, like most Burmese men, wears a longyi, a traditional wraparound skirt. He says if the building's roof isn't fixed properly, \"the rain will leak and destroy the whole structure.\"
-The British, who ruled Burma for decades, constructed most of these buildings in their own image. But Tint Lwin doesn't see them as symbols of oppression; he sees them as part of Myanmar's heritage.
-\"You can't be xenophobic,\" he says, echoing the pragmatism of many here. \"These are our assets. This British architecture is a unique one. Almost all in Myanmar like these buildings.\"
-That includes Maung Nyan, a 19-year-old punk rocker, who lives on the building's fourth floor. He's sitting on the floor of his apartment, wearing a black My Chemical Romance T-shirt and playing the Ramones' \"I Want to Be Sedated\" on an electric guitar. Maung Nyan is rebellious by Burmese standards, but when it comes to construction, he's a traditionalist.
-
-\"Because of the valuable architecture, I prefer this kind of old building to new buildings,\" says Maung Nyan, whose apartment is really a cagelike, cavernous stall with a wire-mesh door. \"I'm also proud to live here. If it's possible, I'd like to stay here until I die.\""
+//           article_text: "Myanmar has opened up its political system, improved its relationship with the West and ushered in a real estate boom in the past several years. That's mostly good news for Yangon, but not for its remarkable architectural heritage, which has come into the cross hairs of developers trying to cash in on rising land prices.
+//
+// \"These buildings are priceless,\" says Tint Lwin, who has taught English in a colonial-era building along the city's Pansodan Street for more than three decades. The building, constructed by a Baghdadi Jewish trader around 1906, has ocher-colored walls and Corinthian columns.
+//
+// Tint Lwin loves the atmosphere of the neighborhood, but worries it won't last. A modern mid-rise is going up across the road. The walls in his building are pitted with black mold, and rain has saturated some ceilings, leaving gaping holes.
+// \"I feel very unhappy because of the negligence,\" says Tint Lwin, who, like most Burmese men, wears a longyi, a traditional wraparound skirt. He says if the building's roof isn't fixed properly, \"the rain will leak and destroy the whole structure.\"
+// The British, who ruled Burma for decades, constructed most of these buildings in their own image. But Tint Lwin doesn't see them as symbols of oppression; he sees them as part of Myanmar's heritage.
+// \"You can't be xenophobic,\" he says, echoing the pragmatism of many here. \"These are our assets. This British architecture is a unique one. Almost all in Myanmar like these buildings.\"
+// That includes Maung Nyan, a 19-year-old punk rocker, who lives on the building's fourth floor. He's sitting on the floor of his apartment, wearing a black My Chemical Romance T-shirt and playing the Ramones' \"I Want to Be Sedated\" on an electric guitar. Maung Nyan is rebellious by Burmese standards, but when it comes to construction, he's a traditionalist.
+//
+// \"Because of the valuable architecture, I prefer this kind of old building to new buildings,\" says Maung Nyan, whose apartment is really a cagelike, cavernous stall with a wire-mesh door. \"I'm also proud to live here. If it's possible, I'd like to stay here until I die.\""
         },
         course_id: 1,
         tool_type: "ART",
